98e612893fdac37258ca2801af34fb1931bd0149
RGGH <pisscompany@gmail.com>
2025-01-20 20:43:02 +0000

    feat: add generics

--- src/bin/generics_tutorial.rs
+++ src/bin/generics_tutorial.rs
@@ -1,5 +1,14 @@
 // src/bin/generics_tutorial.rs
+//
+//
+fn take_2_anything<T:std::ops::Mul<Output = T>>(a:T,b:T)->T{
+    a*b
+}
 fn main() {
     println!("Learning about generics!");
+
+    let res = take_2_anything(3.0, 5.1);
+
+    println!("{:.2?}", res);
 }
 
